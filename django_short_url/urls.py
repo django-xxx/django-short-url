c6dcb18bad8028a4b75b84dd91487a58db43e73d
Brightcells <brightcells@gmail.com>
2019-08-29 12:00:50 +0800

    :art: Add app_name for urls.py

--- django_short_url/urls.py
+++ django_short_url/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import include, url
 from django_short_url import views as surl_views
 
 
+app_name = 'django_short_url'
+
+
 urlpatterns = [
     url(r'^(?P<surl>\w+)', surl_views.short_url_redirect, name='short_url_redirect'),
 ]
